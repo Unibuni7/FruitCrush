99b2545d0c694a89a4ab8181eff35fa8e51306c8
Abdul2010 <abdu0445@edu.easj.dk>
2018-07-17 16:01:12 +0200

    First commit
    Some features works and others don't, i will look at them and fix
    them!!!!!!

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    // how wide i want my board to be.
+    public int width;
+    // how tall i want my board to be.
+    public int height;
+    // My prefab game object.
+    public GameObject tilePrefab;
+    // An array that can contain two variables.
+    private BackgroundTile[,] allTiles;
+    // An array to contain our characters.
+    public GameObject[] characters;
+    public GameObject[,] allCharacters;
+
+    // Use this for initialization
+    void Start () {
+        // My array contains width and height.
+        allTiles = new BackgroundTile[width, height];
+        allCharacters = new GameObject[width, height];
+        SetUp();
+	}
+	
+	private void SetUp()
+    {
+        // two for loops to run through the width and height.
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                // the positon for the width and height.
+                Vector2 tempPosition = new Vector2(i, j);
+                // what it should setup and where, and Quaternion.identity means no rotation.
+                GameObject backgroundTile = Instantiate(tilePrefab,tempPosition,Quaternion.identity) as GameObject;
+                // Here i make my hierarchy in unity more clean.
+                backgroundTile.transform.parent = this.transform;
+                // Here we name our prefab clones.
+                backgroundTile.name = "(" + i + "," + j + ")";
+                // a random character to use from the array.
+                int charactersToUse = Random.Range(0, characters.Length);
+                // position and stuff.
+                GameObject character = Instantiate(characters[charactersToUse], tempPosition, Quaternion.identity) as GameObject;
+                character.transform.parent = this.transform;
+                // the name of the characters.
+                character.name = "(" + i + "," + j + ")";
+                allCharacters[i, j] = character;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characters : MonoBehaviour {
+
+    [Header("Board Variables")]
+    public int column;
+    public int row;
+    public int previousColumn;
+    public int previousRow;
+    public int targetX;
+    public int targetY;
+    public bool isMatched = false;
+    private GameObject otherChararcter;
+    private Board board;
+    private Vector2 firstTouchPosition;
+    private Vector2 finalTouchPosition;
+    private Vector2 tempPosition;
+    public float swipeAngel = 0f;
+
+	// Use this for initialization
+	void Start () {
+        board = FindObjectOfType<Board>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
+        previousColumn = column;
+        previousRow = row;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        targetX = column;
+        targetY = row;
+        FindMatches();
+        if(isMatched == true)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, .2f);
+        }
+        if(Mathf.Abs(targetX - transform.position.x) > .1)
+        {
+            //Moves towards the target.
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        } else
+        {
+            // Directly set the position
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+            board.allCharacters[column, row] = this.gameObject;
+        }
+
+        if (Mathf.Abs(targetY - transform.position.y) > .1)
+        {
+            //Moves towards the target.
+            tempPosition = new Vector2(transform.position.x ,targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            // Directly set the position
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+            board.allCharacters[column, row] = this.gameObject;
+        }
+    }
+
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        if(otherChararcter != null)
+        {
+            if(!isMatched && !otherChararcter.GetComponent<Characters>().isMatched)
+            {
+                otherChararcter.GetComponent<Characters>().row = row;
+                otherChararcter.GetComponent<Characters>().column = column;
+                row = previousRow;
+                column = previousColumn;
+            }
+            otherChararcter = null;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(firstTouchPosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+
+    void CalculateAngle()
+    {
+        swipeAngel = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x)* 180/Mathf.PI;
+        Debug.Log(swipeAngel);
+        MovePieces();
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngel > -45 && swipeAngel <= 45 && column < board.width -1)
+        {
+            // Right swipe.
+            otherChararcter = board.allCharacters[column + 1, row];
+            otherChararcter.GetComponent<Characters>().column -= 1;
+            column += 1;
+
+        } else if (swipeAngel > 45 && swipeAngel <= 135 && row < board.height -1)
+        {
+            // Up swipe.
+            otherChararcter = board.allCharacters[column, row + 1];
+            otherChararcter.GetComponent<Characters>().row -= 1;
+            row += 1;
+
+        } else if ((swipeAngel > 135 || swipeAngel <= -135) && column > 0)
+        {
+            // Left swipe.
+            otherChararcter = board.allCharacters[column - 1, row];
+            otherChararcter.GetComponent<Characters>().column += 1;
+            column -= 1;
+
+        } else if (swipeAngel < -45 && swipeAngel >= -135 && row > 0)
+        {
+            // Down swipe.
+            otherChararcter = board.allCharacters[column, row -1];
+            otherChararcter.GetComponent<Characters>().row += 1;
+            row -= 1;
+        }
+
+        StartCoroutine(CheckMoveCo());
+    }
+
+    void FindMatches()
+    {
+        if(column > 0 && column < board.width -1)
+        {
+            GameObject leftCharacter1 = board.allCharacters[column - 1, row];
+            GameObject rightCharacter1 = board.allCharacters[column + 1, row];
+            if(leftCharacter1.tag == this.gameObject.tag && rightCharacter1.tag == this.gameObject.tag)
+            {
+                leftCharacter1.GetComponent<Characters>().isMatched = true;
+                rightCharacter1.GetComponent<Characters>().isMatched = true;
+                isMatched = true;
+            }
+        }
+
+        if (row > 0 && row < board.height - 1)
+        {
+            GameObject upCharacter1 = board.allCharacters[column, row +1];
+            GameObject downCharacter1 = board.allCharacters[column, row -1];
+            if (upCharacter1.tag == this.gameObject.tag && downCharacter1.tag == this.gameObject.tag)
+            {
+                upCharacter1.GetComponent<Characters>().isMatched = true;
+                downCharacter1.GetComponent<Characters>().isMatched = true;
+                isMatched = true;
+            }
+        }
+    }
+}
